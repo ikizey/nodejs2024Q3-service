44aa10e6fdd2bd1e7e73eeba155e0107f7b1d359
kizey <ikizey@icloud.com>
2024-11-12 01:20:33 +0400

    feat: implement favorites endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UserModule } from './users/user.module';
 import { ArtistModule } from './artists/artist.module';
 import { AlbumModule } from './albums/album.module';
 import { TrackModule } from './tracks/track.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, AlbumModule, TrackModule],
+  imports: [
+    UserModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
