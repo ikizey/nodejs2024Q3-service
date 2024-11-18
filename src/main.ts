1ad5b07376efce4712f9b96434833da0a05f66b6
kizey <ikizey@icloud.com>
2024-11-19 01:15:51 +0400

    fix: read port value from .env

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 
+const port = +process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const api = yaml.load(
     readFileSync('./doc/api.yaml', 'utf8'),
   ) as OpenAPIObject;
   SwaggerModule.setup('doc', app, api);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
